3e99c5f63f5b2d5c5265c3646c0b9dddefe3451a
Josh Forisha <josh@forisha.com>
2025-02-26 12:37:59 -0700

    Add inline option and styling for neu-number

--- neu-number.js
+++ neu-number.js
@@ -1,5 +1,7 @@
 const styles = `
 :host {
+  display: inline-grid;
+  grid-template-columns: repeat(2, auto);
   width: 100%;
 }
 
@@ -12,6 +14,8 @@ input {
   cursor: text;
   font: inherit;
   font-weight: 400;
+  grid-column: 1 / span 2;
+  grid-row-start: 2;
   min-height: var(--control);
   outline: none;
   padding: 0 var(--small);
@@ -29,21 +33,23 @@ input {
 }
 
 label {
-  display: flex;
-  flex-wrap: wrap;
+  font-weight: 500;
+  white-space: nowrap;
+}
 
-  & > span {
-    display: flex;
-    flex-basis: 50%;
+.trailing {
+  justify-self: flex-end;
+}
 
-    &:first-of-type {
-      font-weight: 500;
-    }
+:host(:state(inline)) {
+  align-items: center;
+  column-gap: var(--small);
+  grid-template-columns: min-content max-content min-content;
+}
 
-    &:last-of-type {
-      justify-content: flex-end;
-    }
-  }
+:host(:state(inline)) input {
+  grid-column: unset;
+  grid-row-start: unset;
 }
 
 :host(:state(disabled)) input {
@@ -65,6 +71,7 @@ class NeuNumber extends HTMLElement {
   static observedAttributes = [
     'autofocus',
     'disabled',
+    'inline',
     'leading',
     'max',
     'min',
@@ -81,25 +88,24 @@ class NeuNumber extends HTMLElement {
     const root = this.attachShadow({ mode: 'open' })
     this._internals = this.attachInternals()
 
+    this.leadingLabel = document.createElement('label')
+    this.leadingLabel.classList.add('leading')
+    root.appendChild(this.leadingLabel)
+
     this.input = document.createElement('input')
+    this.input.setAttribute('type', 'number')
     this.input.addEventListener('input', (event) => {
       this.dispatchEvent(new Event('change'))
     })
+    root.appendChild(this.input)
 
-    this.label = document.createElement('label')
-    this.leadingText = document.createElement('span')
-    this.trailingText = document.createElement('span')
+    this.trailingLabel = document.createElement('label')
+    this.trailingLabel.classList.add('trailing')
+    root.appendChild(this.trailingLabel)
 
     const style = document.createElement('style')
     style.textContent = styles
     root.appendChild(style)
-
-    this.input.setAttribute('type', 'number')
-
-    this.label.appendChild(this.leadingText)
-    this.label.appendChild(this.trailingText)
-    this.label.appendChild(this.input)
-    root.appendChild(this.label)
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -120,8 +126,12 @@ class NeuNumber extends HTMLElement {
           this._internals.states.delete('disabled')
         }
         break
+      case 'inline':
+        if (newValue !== null) this._internals.states.add('inline')
+        else this._internals.states.delete('inline')
+        break
       case 'leading':
-        this.leadingText.textContent = newValue
+        this.leadingLabel.textContent = newValue
         break
       case 'max':
       case 'min':
@@ -134,7 +144,7 @@ class NeuNumber extends HTMLElement {
         else this._internals.states.delete('small')
         break
       case 'trailing':
-        this.trailingText.textContent = newValue
+        this.trailingLabel.textContent = newValue
         break
       case 'value':
         this.input.setAttribute('value', newValue)
